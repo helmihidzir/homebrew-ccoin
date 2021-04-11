d91ef71c11d1387b44680757a50bb006f05c7e24
helmihidzir <helmihidzir@gmail.com>
2021-04-11 16:48:32 +0800

    Update version

--- Formula/ccoin.rb
+++ Formula/ccoin.rb
@@ -1,7 +1,7 @@
 class Ccoin < Formula
   desc "Get a crypto coin price"
   homepage "https://github.com/helmihidzir/homebrew-ccoin"
-  version "0.1"
+  version "0.2"
 
   url "https://github.com/helmihidzir/homebrew-ccoin/archive/main.zip", :using => :curl
 
